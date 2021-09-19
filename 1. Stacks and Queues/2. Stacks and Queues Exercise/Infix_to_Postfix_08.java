9680dd0366c83202af4f3640cfbf103ff11b1e14
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-09-19 12:22:29 +0300

    Add files via upload

---
+++ 1. Stacks and Queues/2. Stacks and Queues Exercise/Infix_to_Postfix_08.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Infix_to_Postfix_08 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+        Deque<String> numbersQueue = new ArrayDeque<>();
+        Deque<String> operatorStack = new ArrayDeque<>();
+
+        for (String symbol : input) {
+            if (Character.isDigit(symbol.charAt(0)) || Character.isLetter(symbol.charAt(0))) {
+                numbersQueue.offer(symbol);
+            } else {
+                if (operatorStack.isEmpty()) {
+                    operatorStack.push(symbol);
+                } else {
+                    String lastOperator = operatorStack.peek();
+                    switch (symbol) {
+                        case "+":
+                        case "-":
+                            if (lastOperator.equals("(")) {
+                                operatorStack.push(symbol);
+                            } else {
+                                numbersQueue.offer(operatorStack.pop());
+                                operatorStack.push(symbol);
+                            }
+                            break;
+                        case "*":
+                        case "/":
+                            if (lastOperator.equals("*") || lastOperator.equals("/")) {
+                                numbersQueue.offer(operatorStack.pop());
+                                operatorStack.push(symbol);
+                            } else {
+                                operatorStack.push(symbol);
+                            }
+                            break;
+                        case "(":
+                            operatorStack.push(symbol);
+                            break;
+                        case ")":
+                            while (!operatorStack.peek().equals("(")) {
+                                numbersQueue.offer(operatorStack.pop());
+                            }
+                            //Removing the non-necessary open parenthesis '('
+                            operatorStack.pop();
+                            break;
+                    }
+                }
+            }
+        }
+
+        while (!numbersQueue.isEmpty()) {
+            System.out.print(numbersQueue.poll() + " ");
+        }
+
+        while (!operatorStack.isEmpty()) {
+            System.out.print(operatorStack.pop() + " ");
+        }
+        System.out.println();
+    }
+}
