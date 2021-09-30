cba788f1c444711628e84f01ecec7982009ef67e
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-09-30 21:56:37 +0300

    Add files via upload

---
+++ 5. Functional Programing/1. Functional Programing Lab/Sum_Numbers_02.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Sum_Numbers_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(", ");
+
+        Function<String, Integer> parser = Integer::parseInt;
+
+        int sum = Arrays.stream(input)
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        System.out.printf("Count = %d%n", input.length);
+        System.out.printf("Sum = %d", sum);
+    }
+}
