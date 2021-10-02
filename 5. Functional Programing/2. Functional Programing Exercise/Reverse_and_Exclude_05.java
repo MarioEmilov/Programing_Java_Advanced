ae112d6134944580e0795fc33842b7bab8294c07
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-10-02 15:39:20 +0300

    Add files via upload

---
+++ 5. Functional Programing/2. Functional Programing Exercise/Reverse_and_Exclude_05.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Reverse_and_Exclude_05 {
+    public static void main(String[] args) {
+        //Function<приема, връща> -> apply
+        //Consumer<приема> -> void -> accept
+        //Supplier<връща> -> get
+        //Predicate<приема> -> false / true -> test
+        //BiFunction<приема1, приема2, връща> -> apply
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine(); //"1 2 3 4 5" -> split -> ["1", "2", "3", "4", "5"]
+        List<Integer> numbers = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        //обръщаме списъка
+        Collections.reverse(numbers);
+
+        //премахваме всички числа, които се делят на n
+        //приема число -> true / false
+        Predicate<Integer> checkDivision = number -> number % n == 0;
+        numbers.removeIf(checkDivision);
+
+        numbers.forEach(number -> System.out.print(number + " "));
+    }
+}
