8a01dc5cb63ac23123f2705b69740e1029846216
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-10-17 21:01:01 +0300

    Add files via upload

---
+++ 9. Iterators and Comparators/1. Iterators and Comparators Lab/ComparableBook_03/Main.java
@@ -0,0 +1,17 @@
+package ComparableBook_03;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+
+        if (bookOne.compareTo(bookTwo) > 0) {
+            System.out.printf("%s is before %s%n", bookOne.getTitle(), bookTwo.getTitle());
+        } else if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.printf("%s is before %s%n", bookTwo.getTitle(), bookOne.getTitle());
+        } else {
+            System.out.println("BookComparator_04.Book are equal");
+        }
+    }
+}
